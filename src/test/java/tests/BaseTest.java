9e6668f19fcd60189e54583a3be009a93a09a9ea
vitalichatyrka <vitalichatyrka@gmail.com>
2025-05-09 12:10:21 +0300

    fix for password manager

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,10 +14,12 @@ import pages.LoginPage;
 import pages.ProductsPage;
 
 import java.time.Duration;
+import java.util.Map;
 
 public class BaseTest {
 
     WebDriver driver;
+    ChromeOptions options;
     LoginPage loginPage;
     SoftAssert softAssert;
     ProductsPage productsPage;
@@ -26,8 +29,14 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
-
-        driver = new ChromeDriver();
+            options = new ChromeOptions();
+            options.setExperimentalOption("prefs", Map.of(
+                    "credentials_enable_service", false,
+                    "profile.password_manager_enabled", false
+            ));
+            options.addArguments("--disable-features=PasswordManagerEnableLeakDetection,AutofillServerCommunication,PasswordCheck");
+            options.addArguments("--disable-blink-features=PasswordCredential,CredentialManagerAPI");
+            driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         softAssert = new SoftAssert();
@@ -36,10 +45,6 @@ public class BaseTest {
         cartPage = new CartPage(driver);
         checkoutInformationPage = new CheckoutInformationPage(driver);
         checkoutOverviewPage = new CheckoutOverviewPage(driver);
-
-
-
-
     }
 
     @Test
