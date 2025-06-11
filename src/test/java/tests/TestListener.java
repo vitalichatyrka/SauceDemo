fcf670606cce8e690dccc38326f4dfe8278f0204
vitalichatyrka <vitalichatyrka@gmail.com>
2025-06-11 21:56:28 +0300

    fix test listener

--- src/test/java/tests/TestListener.java
+++ src/test/java/tests/TestListener.java
@@ -1,40 +1,36 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
+import lombok.extern.log4j.Log4j2;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import java.util.concurrent.TimeUnit;
 
-import static tests.AllureUtils.takeScreenshot;
 
-
+@Log4j2
 public class TestListener implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
-        System.out.printf("======================================== STARTING TEST %s ========================================%n", iTestResult.getName());
+        log.info("======================================== STARTING TEST {} ========================================%n", iTestResult.getName());
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        System.out.printf("======================================== FINISHED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
+        log.info("======================================== FINISHED TEST {} Duration:  {} ========================================%n", iTestResult.getName(),
                 getExecutionTime(iTestResult));
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        System.out.printf("======================================== FAILED TEST %s Duration: %ss ========================================%n", iTestResult.getName(),
+        log.info("======================================== FAILED TEST {} Duration:  {} ========================================%n", iTestResult.getName(),
                 getExecutionTime(iTestResult));
-        ITestContext context = iTestResult.getTestContext();
-        WebDriver driver = (WebDriver) context.getAttribute("driver");
-        takeScreenshot(driver);
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
-        System.out.printf("======================================== SKIPPING TEST %s ========================================%n", iTestResult.getName());
+        log.info("======================================== SKIPPING TEST {} ========================================%n", iTestResult.getName());
     }
 
     @Override
